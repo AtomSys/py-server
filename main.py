ea7509e141c5d20b21a53387e537d4500aedc4ee
AtomSys <zcrenshaw1@icloud.com>
2023-10-29 14:21:07 -0400

    added index page

--- main.py
+++ main.py
@@ -8,14 +8,13 @@ serverPort = "8080"
 
 class MyServer(BaseHTTPRequestHandler):
     def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
-        self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title></head>", "utf-8"))
-        self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
-        self.wfile.write(bytes("<body>", "utf-8"))
-        self.wfile.write(bytes("<p>This is an example web server.</p>", "utf-8"))
-        self.wfile.write(bytes("</body></html>", "utf-8"))
+        if self.path == "/":
+            mapg = open("srvf/index.html")
+            mp = mapg.read()
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write(bytes(mp))
 
 if __name__ == "__main__":        
     webServer = HTTPServer((hostName, int(serverPort)), MyServer)
