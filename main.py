09b75b4f57a9573c22e7bb1f3b242c2c048909bf
AtomSys <127058397+AtomSys@users.noreply.github.com>
2023-11-01 10:22:58 -0400

    added clarification

--- main.py
+++ main.py
@@ -3,24 +3,27 @@ import time
 import contextlib
 import mimetypes
 import os.path
-config = open("config.txt", "r")
-configlist = config.read().split("__")
 
+#sets hostname and ports
 hostName = str(open("configs/ip.txt", "r").read())
 serverPort = "8080"
 
 class MyServer(BaseHTTPRequestHandler):
     def do_GET(self):
         if self.path == "/":
+            #gets "index.html"
             mapg = open("files/index.html")
             mp = mapg.read()
+            #sends page to user
             self.send_response(200)
             self.send_header("Content-type", "text/html")
             self.end_headers()
             self.wfile.write(bytes(mp, "utf-8"))
         elif os.path.exists("files" + self.path):
+            #gets page html
             pgop = open("files" + self.path, "r") 
             pg = pgop.read()
+            #sends page
             self.send_response(200)
             self.send_header("Content-type", "text/html")
             self.end_headers()
